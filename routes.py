955927f012f44cb38bf046aba98cbc0f7ba7d119
Pushkar Sheth <pushkar@briskfyr.com>
2018-04-28 21:18:46 +0530

    file reader fucntionality

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -6,10 +7,24 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route('/start')
+def start():
+    return render_template("start.html")
+	
 @app.route('/about')
 def about():
     return render_template("about.html")
 
+@app.route("/sigsolver", methods = ['GET', 'POST'])
+def sigsolver():
+    if request.method=='POST':
+        temp1=request.files['filedata']
+        print(temp1)
+        #df = pd.DataFrame()
+        df = pd.read_csv(request.files.get("filedata"))
+        print(df)
+        return render_template("sigsolver.html")
+		
 if __name__=="__main__":
     app.run(debug=True)
 
