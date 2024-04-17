40f16b2799c107d75fbc8c209ecc9a49a6ea57b4
Pushkar Sheth <ph@sheth.cc>
2024-04-17 22:30:47 +0530

    updated setup.py file with description

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -2,13 +2,20 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    description = f.read()
+
+
 setup(
     name='siglyser',
-    version='0.1',
+    version='0.11',
     packages=find_packages(),
     install_requires=[
         # Add dependencies here.
 
         
     ],
+
+    long_description=description,
+    long_description_content_type="text/markdown",
 )
